ce5d7b999126cdb2fa1d389acabaa6b240b9bae2
netxvul <netxvul@noreply.gitcode.com>
2026-03-28 14:52:42 +0800

    Initial Commit

---
+++ DefaultOpener/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DefaultOpener
+//
+//  Created by Kun Peng on 2026/3/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DefaultOpener/DefaultOpenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefaultOpenerApp.swift
+//  DefaultOpener
+//
+//  Created by Kun Peng on 2026/3/28.
+//
+
+import SwiftUI
+
+@main
+struct DefaultOpenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
